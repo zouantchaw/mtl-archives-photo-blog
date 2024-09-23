14f9200317267aa99a4c9bde4207e6a0fa98366b
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2024-09-22 20:44:56 -0400

    Refactor dynamic import and function signature in MapPage

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,12 +1,14 @@
-import dynamic from 'next/dynamic';
-import { Suspense } from 'react';
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
-import { getPhotosCached, getPhotosCountCached } from '@/photo/cache';
-import { PaginationParams, getPaginationForSearchParams } from '@/site/pagination';
+import {
+  PaginationParams,
+  getPaginationForSearchParams,
+} from "@/site/pagination";
 
-const MapClient = dynamic(() => import('@/components/MapClient'), {
+const MapClient = dynamic(() => import("@/components/MapClient"), {
   ssr: false,
-  loading: () => <MapSkeleton />
+  loading: () => <MapSkeleton />,
 });
 
 function MapSkeleton() {
@@ -17,16 +19,7 @@ function MapSkeleton() {
   );
 }
 
-export default async function MapPage({ searchParams }: PaginationParams) {
-  const { offset, limit } = getPaginationForSearchParams(searchParams, 12);
-  const [
-    photos,
-    count,
-  ] = await Promise.all([
-    // Make homepage queries resilient to error on first time setup
-    getPhotosCached({ limit }).catch(() => []),
-    getPhotosCountCached().catch(() => 0),
-  ]);
+export default function MapPage() {
   return (
     <div className="w-full h-[calc(100vh-6rem)] flex flex-col md:flex-row">
       <Suspense fallback={<MapSkeleton />}>
